1e114474a6597aba1f6770ae8a3de1fdde31fb38
rayeebac <rayeeback123@gmail.com>
2024-08-13 12:55:17 +0530

    added

--- ecommerceproject/shop/views.py
+++ ecommerceproject/shop/views.py
@@ -10,13 +10,13 @@ def allProdCat(request,c_slug=None):
     c_page=None
     products_list=None
     if c_slug!=None:
-        c_page = get_object_or_404(Category, slug=c_slug)
+        c_page = get_object_or_404(Category,slug=c_slug)
         products_list = Product.objects.all().filter(category=c_page, available=True)
     else:
         products_list = Product.objects.all().filter(available=True)
     paginator = Paginator(products_list, 6)
     try:
-        page = int(request.GET.get('page', 1))
+        page = int(request.GET.get('page', '1'))
     except:
         page = 1
     try:
